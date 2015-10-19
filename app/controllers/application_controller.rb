a0e769f017125d1cb382a658131dcf27a97d0a4c
Volney Tejeda <volneytejeda10@gmail.com>
2015-10-19 19:44:30 -0400

    add conditions to abilites

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  check_authorization
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:warning] = exception.message
     redirect_to root_path
   end
-  
+
   private
 
   def current_user
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019221215) do
+ActiveRecord::Schema.define(version: 20151019233336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.boolean  "ongoing",    default: true
   end
+
+  add_index "projects", ["ongoing"], name: "index_projects_on_ongoing", using: :btree
 
 end
